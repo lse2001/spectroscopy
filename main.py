eca6bb016ef18728e8e474f0c2c1e52e3f5e0357
lse2001 <lucas.seitz@gmail.com>
2025-04-03 22:05:18 -0700

    Display both plt and spectral imshow() outputs. plt.imshow() might be overwriting previously created objects.

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ image = spectral.open_image(envi_file)  # Manually add the parameter "byte order
 data = image.load()
 # data is ImageArray type (similar to NumPy array)
 
-# imshow expects np.ndarray or SpyFile object, that is why we have to load the data from BipFile first to be ImageArray, which is similar to NumPy array
-plt.imshow(data[:, :, [40, 60, 40]])  # RGB for displaying the relative intensity of absorption
-plt.show()
+
+plt.imshow(data[:,:,[0,0,0]])  # AxesImage type
+spectral.imshow(data, bands=(0,0,0))  # ImageView type
+# spectral.imshow expects numPy array-like object or SpyFile object, that is why we have to load the data from BipFile first to be ImageArray, which is similar to NumPy array
+
+plt.show(block="True")
+
+"""something about the imshow() method of spectral package vs imshow() method of plt package is changing the objects that are being displayed by plt.show()
+I think this issues comes from calling plt.imshow() after spectral.imshow() has already created objects. plt.imshow() deletes the previously created objects"""
+
