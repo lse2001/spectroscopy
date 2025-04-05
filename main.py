281699dbe05da521f0d5749eefe33749092e9d08
lse2001 <lucas.seitz@gmail.com>
2025-04-04 23:14:51 -0700

    Added comments further clarifying conflicts between the imshow() methods of the plt vs. spectral packages.

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ data = image.load()
 # data is ImageArray type (similar to NumPy array)
 
 
-plt.imshow(data[:,:,[0,0,0]])  # AxesImage type
-spectral.imshow(data, bands=(0,0,0))  # ImageView type
+plt.imshow(data[:,:,[0,0,0]])  # AxesImage type # Overwrites CURRENT axes
+spectral.imshow(data, bands=(0,0,0))  # ImageView type # Creates NEW figure
 # spectral.imshow expects numPy array-like object or SpyFile object, that is why we have to load the data from BipFile first to be ImageArray, which is similar to NumPy array
 
 plt.show(block="True")
@@ -18,3 +18,12 @@ plt.show(block="True")
 """something about the imshow() method of spectral package vs imshow() method of plt package is changing the objects that are being displayed by plt.show()
 I think this issues comes from calling plt.imshow() after spectral.imshow() has already created objects. plt.imshow() deletes the previously created objects"""
 
+"""
+spectral.imshow()
+Creates an ImageView object (from the spectral package) in a new figure
+plt.imshow()
+Creates an AxesImage object (from Matplotlib) in the current axes
+
+spectral.imshow(data, bands=(0,0,0))  # Creates NEW figure
+plt.imshow(data[:,:,[0,0,0]])         # Overwrites CURRENT axes
+"""
